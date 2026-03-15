9362652b5cad85510666e6c0a3309a5968d6cd39
Pratik_Dhayagude <pratikdhaygude21@gmail.com>
2026-03-15 14:45:27 +0530

    First Commit

---
+++ Marvellous_Study_Tracker.java
@@ -0,0 +1,291 @@
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.io.*;
+
+class StudyLog
+{
+    private LocalDate Date;
+    private String subject;
+    private double duration;
+    private String description;
+
+    public StudyLog(LocalDate a, String b,double c,String d)
+
+    {
+        this.Date = a;
+        this.subject = b;
+        this.duration = c;
+        this.description = d;
+
+    }
+    public LocalDate getDate()
+    {
+        return this.Date;
+    }
+
+    public String getSubject()
+    {
+        return this.subject;
+    }
+    public double getduration()
+    {
+        return this.duration;
+    }
+
+    public  String getDescription()
+    {
+        return this.description;
+    }
+    @Override
+    public String toString()
+    {
+        return Date+"|"+subject+"|"+duration+"|"+description;
+
+    }
+}
+//done
+class StudyTracker
+{
+    public ArrayList <StudyLog>database = new ArrayList<StudyLog>();
+    public void InsertLog()
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("----------------Enter Valid details of your study---------------");
+        System.out.println("----------------------------------------------------------------");
+
+        LocalDate dateobj = LocalDate.now();
+
+        System.out.println("Please Enter the name of subject like c/c++/java/python");
+        String sub = sobj.nextLine();
+        
+        System.out.println("Enter the time period of study in hours");
+        double dur = sobj.nextDouble();
+
+        System.out.println("please provide the description of your study");
+        String des = sobj.next();
+
+        StudyLog studyobj = new StudyLog(dateobj, sub, dur, des);
+
+        database.add(studyobj);
+
+        System.out.println("Study log gets stored successfully..");
+
+        System.out.println("----------------------------------------------------------------");
+
+        sobj.close();
+
+
+
+
+    }
+    public void DisplayLog()
+    {
+        
+
+         System.out.println("----------------------------------------------------------------");
+         if(database.isEmpty())
+         {
+           
+            System.out.println("---------------------Nothing to Display-------------------------");
+            System.out.println("----------------------------------------------------------------");
+            return;
+         }
+         System.out.println("------------Log report of arvellous study track-----------------");
+         System.out.println("----------------------------------------------------------------");
+
+         for(StudyLog s : database)
+         {
+            System.out.println(s);
+
+         }
+        System.out.println("----------------------------------------------------------------");
+
+    }
+    public void ExportCSV()
+    {
+         if(database.isEmpty())
+         {
+            System.out.println("----------------------------------------------------------------");
+            System.out.println("---------------------Nothing to Export--------------------------");
+            System.out.println("----------------------------------------------------------------");
+            return;
+         }
+
+         String Filename = "MarvellousStudyTracker.CSV";
+
+         try(FileWriter fwobj = new FileWriter(Filename))
+         {
+            fwobj.write("date,subject,duration.,description\n");
+            for(StudyLog s : database)
+            {
+                fwobj.write(s.getDate()+","+
+                            s.getSubject().replace(","," ")+
+                            ","+s.getduration()+
+                            ","+s.getDescription().replace(","," ")+"\n");
+
+                System.out.println("Data gets exported in csv"+Filename);
+
+            }
+         }
+         catch(Exception eobj )
+         {
+            System.out.println("Exception occured in csv handling");
+
+         }
+    }
+
+    public void SummaryByDate()
+    {
+         System.out.println("----------------------------------------------------------------");
+         if(database.isEmpty())
+         {
+            System.out.println("--------------Nothing to Display as database is empty-----------");
+            System.out.println("----------------------------------------------------------------");
+            return;
+         }
+         LocalDate lobj = null;
+         double d = 0.0,old =0.0;
+         System.out.println("------------Summary by date from study tracker------------------");
+         System.out.println("----------------------------------------------------------------");
+
+         TreeMap <LocalDate,Double>tobj = new TreeMap<LocalDate,Double>();
+
+         for(StudyLog sobj : database)
+         {
+            lobj = sobj.getDate();
+            d = sobj.getduration();
+            if(tobj.containsKey(lobj))
+            {
+                old = tobj.get(lobj); 
+                tobj.put(lobj,d+old);
+            }
+            else
+            {
+                tobj.put(lobj, d);
+            }
+         }
+         // Display the details as per date
+         for(LocalDate l : tobj.keySet())
+         {
+            System.out.println("date :"+l+"Total study duration:"+tobj.get(l));
+         }
+         System.out.println("----------------------------------------------------------------"); 
+        
+    }
+    public void SummaryBySubject()
+    {
+         System.out.println("----------------------------------------------------------------");
+         if(database.isEmpty())
+         {
+            System.out.println("--------------Nothing to Display as database is empty-----------");
+            System.out.println("----------------------------------------------------------------");
+            return;
+         }
+         String s = null;
+         double d = 0.0,old =0.0;
+         System.out.println("Summary by subject from study tracker");
+         System.out.println("----------------------------------------------------------------");
+
+         TreeMap <String,Double>tobj = new TreeMap<String,Double>();
+
+         for(StudyLog sobj : database)
+         {
+            s = sobj.getSubject();
+            d = sobj.getduration();
+            if(tobj.containsKey(s))
+            {
+                old = tobj.get(s); 
+                tobj.put(s,d+old);
+            }
+            else
+            {
+                tobj.put(s, d);
+            }
+         }
+         // Display the details as per subject 
+         for(String str : tobj.keySet())
+         {
+            System.out.println("Subject :"+str+"Total study duration:"+tobj.get(str));
+         }
+         System.out.println("----------------------------------------------------------------");
+
+    }
+    
+}
+
+class Marvellous_study_tracker
+
+{
+    public static void main(String A[]) 
+    {
+       Scanner sobj = new Scanner(System.in);
+       StudyTracker stobj = new StudyTracker();
+
+
+       System.out.println("----------------------------------------------------------------");
+       System.out.println("--------------Welocome to marvellous study tracker--------------");
+       System.out.println("----------------------------------------------------------------");
+
+       int iChoise =0;
+
+       do
+       {
+            System.out.println("Please select approprite optation");
+            System.out.println("1:Insert new study log");
+            System.out.println("2:view all study log");
+            System.out.println("3:Export study log to csv file");
+            System.out.println("4:Summary of study log by date");
+            System.out.println("5:Summary of study log by subject");
+            System.out.println("6:Exit they application");
+
+            iChoise = sobj.nextInt();
+
+            switch (iChoise)
+             {
+                case 1:
+                        // insert new study log
+                        stobj.InsertLog();
+                        break;
+                    
+                case 2 :
+                        // view all study log 
+                        stobj.DisplayLog();
+                        break;
+                        
+                case 3 :
+                       //Export study log to csv file
+                        stobj.ExportCSV();
+                        break;
+                case 4 :
+                    //Summary of study log by date
+                     stobj.SummaryByDate();
+                     break;
+
+                case 5 :
+                    //Summary of study log by subject
+                    stobj.SummaryBySubject();
+                    break;
+
+                case 6 :
+                    // Exit the application
+                     System.out.println("----------------------------------------------------------------");
+                     System.out.println("-----------Thanku for using Marvellous study tracker------------");  
+                     System.out.println("----------------------------------------------------------------");
+                    break;
+            
+                default:
+                    System.out.println("Please Enter the vaild optation");
+                    break;
+            }
+
+       }while(iChoise != 6);// End of do while
+
+       sobj.close();
+
+
+          
+    }// End of main 
+}//end of starter class
